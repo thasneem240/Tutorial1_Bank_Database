7e7579e2f7335071bf26dfa716a4cc6c4c8465c3
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-17 14:34:09 +0530

    Created Another project called remote Server and created a BankServerInterface

---
+++ RemoteServer/BankServerInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteServer
+{
+    [ServiceContract]
+    public interface BankServerInterface
+    {
+        [OperationContract]
+        int GetNumEntries();
+
+        [OperationContract]
+        void GetValuesForEntry(int index, out uint accNo, out uint pin, out int bal,
+            out string fName, out string lName);
+    }
+}
