120222daa42293b79e37dc4ea9bad796db6adff6
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-17 22:33:34 +0530

    Completed The Task 1 Random Generator for names and others. Overall Task 1- Task 3 Fully Completed.

--- Tutorial1_Simple_WPF_APP/DatabaseGenerator.cs
+++ Tutorial1_Simple_WPF_APP/DatabaseGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,35 +10,53 @@ namespace Bank_Database
     internal class DatabaseGenerator
     {
         static void Main() { }
+
+        /* Random Object to Create random Numbers and Strings */
+
+        private Random rand = new Random();
+
+        private string[] arrOfFirstName = { "Mohamed","Wade", "Dave", "Seth", "Ivan", "Riley", "Gilbert",
+            "Jorge", "Dan", "Brian", "Roberto", "Ramon", "Miles", "Liam", "Nathaniel", "Ethan", "Lewis",
+            "Milton", "Claude", "Joshua", "Glen", "Harvey", "Blake", "Antonio", "Connor", "Julian", "Aidan"};
+
+        private string[] arrOfLastName = {"Thasneem", "Terry", "Shelton", "Miles", "Lucas", "Fletcher",
+            "Parks", "Norris", "Guzman", "Daniel", "Newton", "Potter", "Francis", "Erickson", "Norman", 
+            "Moody", "Lindsey", "Gross", "Sherman", "Simon", "Jones", "Brown", "Garcia", "Rodriguez", 
+            "Lee", "Young", "Hall" };
+
+
+        
         private string GetFirstname() 
         {
-            string name = "Mohamed";
-            return name;
+            int index = rand.Next(arrOfFirstName.Length);
+            string fName = arrOfFirstName[index];
+            return fName;
         }
 
         private string GetLastname()
         {
-            string name = "Thasneem";
-            return name;
+            int index = rand.Next(arrOfLastName.Length);
+            string lName = arrOfLastName[index];
+            return lName;
         }
 
         private uint GetPIN()
         {
-            uint pinNo = 12;
+            uint pinNo = Convert.ToUInt32(rand.Next(10000)); // Generate a number between 0 and 9999
             return pinNo;
         }
 
 
         private uint GetAcctNo() 
         {
-            uint accNo = 3456756768;
+            uint accNo = Convert.ToUInt32(rand.Next(1000000,10000000));
             return accNo;
         }
 
 
         private int GetBalance() 
         {
-            int accBal = 5667889;
+            int accBal = rand.Next(1000, 1000000000);
             return accBal;
         }
 
