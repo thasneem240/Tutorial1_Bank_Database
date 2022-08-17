c970ba3f8fd5ed56335f03ef109fdc53de20c4d3
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-17 12:10:59 +0530

    DatabaseStorage class created and completed

---
+++ Tutorial1_Simple_WPF_APP/DatabaseStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1_Simple_WPF_APP
+{
+    internal class DatabaseStorage
+    {
+        public uint accNo;
+        public uint pin;
+        public int balance;
+        public string firstName;
+        public string lastName;
+
+        public DatabaseStorage()
+        {
+            accNo = 0;
+            pin = 0; 
+            balance = 0;
+            firstName = "";
+            lastName = "";
+        }
+    }
+}
