bf47731fefee25ddbc12b4abb537e264715c9934
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-17 11:04:56 +0530

    Add project files.

---
+++ Tutorial1_Simple_WPF_APP/BankDetails.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Database
+{
+    internal class BankDetails
+    {
+        private string GetFirstname() 
+        {
+            string name = "Mohamed";
+            return name;
+        }
+
+        private string GetLastname()
+        {
+            string name = "Thasneem";
+            return name;
+        }
+
+        private uint GetPIN()
+        {
+            uint pinNo = 1234;
+            return pinNo;
+        }
+
+
+        private uint GetAcctNo() 
+        {
+            uint accNo = 3456756768;
+            return accNo;
+        }
+
+
+        private int GetBalance() 
+        {
+            int accBal = 5667889;
+            return accBal;
+        }
+
+
+
+
+    }
+}
