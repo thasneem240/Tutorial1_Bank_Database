cf00ff456c75d8baa11daec0346e8c3bc9339e93
Thasneem Abdul Saleem Mohamed Thasneem <abdulsal@student.curtin.edu.au>
2022-08-17 20:10:30 +0530

    Created code for Client in MainWindow

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using RemoteServer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,26 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BankServerInterface foob;
+
         public MainWindow()
         {
+            // Setup the Window
             InitializeComponent();
+
+            /* This is a factory that generates remote connections to our remote class. This 
+               is what hides the RPC stuff! */
+            ChannelFactory<RemoteServer.BankServerInterface> foobFactory;
+            NetTcpBinding tcp = new NetTcpBinding();
+
+            //Set the URL and create the connection!
+            string URL = "net.tcp://localhost:8100/DataService";
+
+            foobFactory = new ChannelFactory<BankServerInterface>(tcp,URL);
+            foob = foobFactory.CreateChannel();
+
+            //Also, tell me how many entries are in the DB.
+            pTotalItem.Text = foob.GetNumEntries().ToString();
         }
     }
 }
